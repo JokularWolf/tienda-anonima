d7e660d569130240e5d697f21aa2845bc6f5f873
DevChris <developer@asjurfinlegaltech.com>
2024-09-24 00:59:31 -0500

    feacture-categoria: Actualizacion de rutas

--- persistencia/CategoriaDAO.php
+++ persistencia/CategoriaDAO.php
@@ -1,24 +1,17 @@
 <?php
-
 class CategoriaDAO{
-
     private $idCategoria;
     private $nombre;
-
-
-    public function __construct($idCategoria=0, $nombre="")
-    {
-        $this ->idCategoria = $idCategoria;
+    
+    public function __construct($idCategoria=0, $nombre=""){
+        $this -> idCategoria = $idCategoria;
         $this -> nombre = $nombre;
-
     }
-
+    
     public function consultarCategoria(){
 
         return "SELECT idCategoria,nombre FROM categoria";
     }
-
 }
-
 
 ?>
