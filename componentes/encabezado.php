d76ecd0b77bf756001c0b22b0390a6ecfbbcdd6f
DevChris <developer@asjurfinlegaltech.com>
2024-10-15 13:43:22 -0500

    formulario crear productos

---
+++ componentes/encabezado.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <style>
+        .material-symbols-rounded {
+            font-variation-settings:
+            'FILL' 1,
+            'wght' 400,
+            'GRAD' 0,
+            'opsz' 24
+        }
+        .centered-form {
+            width: 50%;
+            margin: 0 auto;
+        }
+        .full-page {
+            background-color: #8A0808;
+            padding: 30px;
+            width: 100vw;
+        }
+
+        .nav-item a {
+            color: white;
+        }
+
+        .footer {
+            background-color: #8A0808;
+            padding: 20px;
+            color: white;
+            text-align: center;
+            font-size: 14px;
+            margin-top: auto;
+            
+        }
